37ca8b9293078bbaa77627e5c2578cfcb0e012d8
DimamonDESKTOP <dimamon02@gmail.com>
2024-03-29 13:48:02 +0200

    done

--- dz27.03/Program.cs
+++ dz27.03/Program.cs
@@ -1,1 +1,59 @@
-﻿
+﻿using System;
+
+public class Context
+{
+    private IStrategy strategy;
+
+    public Context(IStrategy strategy)
+    {
+        this.strategy = strategy;
+    }
+
+    public void ExecuteStrategy()
+    {
+        strategy.Execute();
+    }
+
+    public void SetStrategy(IStrategy strategy)
+    {
+        this.strategy = strategy;
+    }
+}
+
+public interface IStrategy
+{
+    void Execute();
+}
+
+public class ConcreteStrategy1 : IStrategy
+{
+    public void Execute()
+    {
+        Console.WriteLine("Виконується конкретна стратегія 1");
+    }
+}
+
+public class ConcreteStrategy2 : IStrategy
+{
+    public void Execute()
+    {
+        Console.WriteLine("Виконується конкретна стратегія 2");
+    }
+}
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.InputEncoding = System.Text.Encoding.Unicode;
+        Console.OutputEncoding = System.Text.Encoding.Unicode;
+
+        Context context = new Context(new ConcreteStrategy1());
+
+        context.ExecuteStrategy();
+
+        context.SetStrategy(new ConcreteStrategy2());
+
+        context.ExecuteStrategy();
+    }
+}
